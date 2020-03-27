2535467f20cd9ad46a200c780ec56d2752b69769
Eleanor Burgess <Eleanor.C.Burgess0940@gmail.com>
2020-03-27 14:42:03 -0400

    added functionality to past, present and future and added style elements

--- Assets/main.js
+++ Assets/main.js
@@ -35,10 +35,15 @@ window.onload = function() {
   day = d.getDay();
   currentWeekday = weekdays[day];
   $("#currentWeekday").html(currentWeekday);
+
+  hour = d.getHours();
+  console.log(hour);
+  currentHour = hour;
+  console.log(currentHour);
 };
 
-var hour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var workDayHour = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+var militaryHour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 for (i = 0; i < workDayHour.length; i++) {
   var newRow = $("<row>");
@@ -46,12 +51,13 @@ for (i = 0; i < workDayHour.length; i++) {
   var timeCol = $("<div>");
   var inputCol = $("<div>");
   newRow.append(timeCol, inputCol);
-  timeCol.attr("class", "col-2 float-left");
+  timeCol.attr("class", "col-2 float-left hour");
   timeCol.html("<h1>" + workDayHour[i] + "</h1>");
   timeCol.innerhtml;
   inputCol.attr("class", "col-10 float-left");
   inputCol.html("<textarea rows='4'></textarea>");
   newRow.attr("class", "row hour");
+  newRow.attr("id", militaryHour[i]);
   if (workDayHour[i] < 9) {
     timeCol.text(workDayHour[i] + "PM");
   } else if (workDayHour === 12) {
@@ -60,3 +66,19 @@ for (i = 0; i < workDayHour.length; i++) {
     timeCol.text(workDayHour[i] + "AM");
   }
 }
+
+$("row").each(function() {
+  var getId = parseInt($(this).attr("id"));
+  console.log("id- " + getId);
+  var timeOfDay = parseInt(moment().format("H"));
+  if (getId < timeOfDay) {
+    console.log("Past");
+    $(this).addClass("past");
+  } else if (getId > timeOfDay) {
+    console.log("Future");
+    $(this).addClass("future");
+  } else {
+    console.log("Present");
+    $(this).addClass("present");
+  }
+});
